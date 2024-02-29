4dffec872e7d760ba3eac765857ae547178b86c4
jalenlum <808jalen@gmail.com>
2024-02-29 13:05:08 -1000

    started project setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    entry : {
+      index : './src/webpack-render/index.js',
+   
+    },
+    output: {
+      filename:'[name].js',
+      path: path.resolve(__dirname,'dist')
+    },
+    devServer: {
+      port: 3010,
+      watchContentBase: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(?:js|jsx|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-react',]
+              ]
+            }
+          }
+        }
+      ]
+    },
+   plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, './.env'),
+      safe: true,
+      systemvars: true,
+    }),
+  ],
+};
