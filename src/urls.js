f901aeee796a95ab462d613b0118d6787f268e3c
Jalen Lum <808jalen@gmail.com>
2024-06-25 23:52:12 -1000

    removed everything

--- src/urls.js
+++
@@ -1,1 +0,0 @@
-export const rootUrl = "https://emailist.com/api/";
--- webpack.prod.js
+++
@@ -1,7 +0,0 @@
-const {merge} = require('webpack-merge')
-const webpackConfig = require('./webpack.config')
-
-module.exports = merge(webpackConfig,{
-    mode:"production",
-    watch:false
-})
